8b3bfd8b4a2179e50e087a3b5dfb06a7ebeb09ca
stevenhuang33231 <56376460+stevenhuang33231@users.noreply.github.com>
2020-04-19 22:08:26 +0800

    ver 1.0.0.1
    modified #1

--- Ch16_3_2/Ch16_3_2/Form1.cs
+++ Ch16_3_2/Ch16_3_2/Form1.cs
@@ -16,5 +16,10 @@ namespace Ch16_3_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Press Button";
+        }
     }
 }
